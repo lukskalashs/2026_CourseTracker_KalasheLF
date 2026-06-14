f48fceada6fd2a7f4a72e70145014e6777d90dab
Lukhanyo Kalashe <kalashslukhanyo@gmail.com>
2026-06-14 16:34:17 +0200

    Part 3 published:  Fix Local time to Utc.now for Postgres

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -34,7 +34,7 @@ namespace API.Services
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(15),
+                Expires = DateTime.UtcNow.AddDays(15),
                 SigningCredentials = creds
             };
 
